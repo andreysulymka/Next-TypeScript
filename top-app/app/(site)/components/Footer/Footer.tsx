68eefed60b088119fe89c63c6b18ed0474f7a2bf
andreysulymka <andreysulymka5@gmail.com>
2023-12-17 19:09:13 +0200

    add styles for footer

--- top-app/app/(site)/components/Footer/Footer.tsx
+++ top-app/app/(site)/components/Footer/Footer.tsx
@@ -1,10 +1,17 @@
 import FooterProps from "./Footer.props";
+import styles from './Footer.module.css';
+import cn from 'classnames';
+import {format} from 'date-fns'
 
-const Footer = ({...props }:FooterProps):JSX.Element => {
+const Footer = ({className, ...props }:FooterProps):JSX.Element => {
     return (
-        <div {...props}>
-            Footer
-        </div>
+        <footer className={cn(className, styles.footer)} {...props}>
+            <div>
+               OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены
+            </div>
+            <a href="#" target="_blank">Користувацьке погодження</a> 
+            <a href="#" target="_blank">Політика конфіденційності</a> 
+        </footer>
  )    
 }
 
